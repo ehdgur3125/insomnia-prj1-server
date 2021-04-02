e3e900a99595851332c30fd4786b156d59d27b91
ehdgur3125 <ehdgur3125@naver.com>
2021-04-02 15:54:39 +0900

    [Main] close #6

--- controllers/main/item.js
+++ controllers/main/item.js
@@ -3,7 +3,19 @@ const models=require('../../models');
 module.exports=async(req,res)=>{
   try{
     const item=await models.Item.findByPk(req.params.itemId,{
-      include:models.Option
+      attributes:[
+        ['id','itemId'],
+        'text',
+        [models.Sequelize.literal(`(select COUNT(*) from Likes where Likes.itemId=${req.params.itemId})`),'likes'],
+        [models.Sequelize.literal(`(select SUM(quantity) from ListItems join Options on ListItems.optionId=Options.id where Options.itemId=${req.params.itemId})`),'purchases']
+      ],
+      include:{
+        model:models.Option,
+        attributes:['id','text','price'],
+        where:{
+          state:"onSale"
+        }
+      }
     });
     res.send(item);
   }
